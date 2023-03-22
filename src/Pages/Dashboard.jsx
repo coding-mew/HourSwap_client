2f09dd817b918d1d1c0733ae5036209b9fbc9843
Pterodactylae <leonie.schwitzkowski@dci-student.de>
2023-03-22 10:40:59 +0100

    implemented navigation and worked on dashboard

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,27 +1,34 @@
-import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { useGameContext } from "../global/Context";
-import { logroutes } from "../routes/routes";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function HomePage() {
-  const { setShowNavbar} =
-    useGameContext();
-
-
-  const navigate = useNavigate();
-
+const UserDashboard = () => {
+  const [userData, setUserData] = useState(null);
+  
   useEffect(() => {
-    setShowNavbar(false);
-    logroutes();
-  },[]);
+    axios.get('/api/userdata')
+      .then(response => setUserData(response.data))
+      .catch(error => console.log(error));
+  }, []);
 
-  // const handleEnterClick = () => {
-  //   navigate("/choose_quiz");
-  // };
   return (
-    <div className="home_container">
-    Hello I Am Dashboard
+    <div className="flex flex-col items-center">
+      {userData && (
+        <>
+          <img src={userData.profilePicture} alt="Profile" className="w-20 h-20 rounded-full mb-4" />
+          <h2 className="text-2xl font-bold">{userData.username}</h2>
+          <p className="text-lg mb-4">{userData.hourTokens} Hour Tokens</p>
+          <div className="flex space-x-4">
+            <button className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded">
+              Button 1
+            </button>
+            <button className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded">
+              Button 2
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
-}
-export default HomePage;
+};
+
+export default UserDashboard;
