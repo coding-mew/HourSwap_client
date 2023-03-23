fa6fec15205fc8aa9c740cdb27d9ddf584a6a29b
Pterodactylae <leonie.schwitzkowski@dci-student.de>
2023-03-23 11:06:48 +0100

    sticky footer at bottom

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className=" bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+    <footer className=" bg-white rounded-lg shadow m-4 dark:bg-gray-800 fixed bottom-0 w-full" >
       <div className="w-full mx-auto container md:p-6 p-4 md:flex md:items-center md:justify-between">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2023{" "}
